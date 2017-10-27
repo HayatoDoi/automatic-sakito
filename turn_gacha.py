34627e4223f5b42deb14e338991bc6264108e7c1
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-10-27 09:43:54 +0900

    ガチャ回すスクリプトの追加

--- turn_gacha.py
+++ turn_gacha.py
@@ -1,38 +1,14 @@
-import requests
 import config
-from bs4 import BeautifulSoup 
+import SakitoScrap
 
 def main():
-  scraping(config.sakito['email'], config.sakito['password'])
+  sakitoScrap = SakitoScrap.SakitoScrap(config.sakito['email'], config.sakito['password'])
 
-def scraping(email,password):
-  session = requests.Session()
-  # get authenticity_token
-  response = session.get('https://sakito.cirkit.jp/user/sign_in')
-  soup = BeautifulSoup(response.text, 'html.parser')
-  authenticity_token = soup.body.findAll('form')[0].find(attrs={'name':'authenticity_token'})['value']
-  # login
-  login_payload = {
-    'utf8': '✓',
-    'authenticity_token': authenticity_token,
-    'user[email]': email,
-    'user[password]': password,
-    'user[remember_me]': '0',
-    'commit': 'ログイン'
-  }
-  session.post('https://sakito.cirkit.jp/user/sign_in', data=login_payload)
+  beforePoint = sakitoScrap.getPoint()
+  sakitoScrap.gacha()
+  afterPoint = sakitoScrap.getPoint()
 
-  # がちゃを回す
-  response = session.get('https://sakito.cirkit.jp/user/point/new')
-  soup = BeautifulSoup(response.text, 'html.parser')
-  authenticity_token = soup.head.find(attrs={'name':'csrf-token'})['content']
-  gacha_payload = {
-    '_method': 'post',
-    'authenticity_token': authenticity_token,
-  }
-  response = session.post('https://sakito.cirkit.jp/user/point', data=gacha_payload)
-
-  print(gacha_payload)
+  print(afterPoint - beforePoint)
 
 if __name__ == '__main__':
   main()
