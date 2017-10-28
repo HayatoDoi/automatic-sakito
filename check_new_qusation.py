1a56444545bbf5ee9a3523b2274ec03af2184bb5
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-10-28 09:56:23 +0900

    新規アンケートがない場合、通知を送らないようにする。

--- check_new_qusation.py
+++ check_new_qusation.py
@@ -4,14 +4,16 @@ import slackweb
 
 def main():
   sakitoScrap = SakitoScrap.SakitoScrap(config.sakito['email'], config.sakito['password'])
+  questionList = sakitoScrap.checkNewQusation()
 
+  #新規アンケートがあればslackに通知を送る
   slackUrl = config.slack['webhooksUrl']
-
-  sendText = '新規アンケートが追加されました。\n'
-  for test in sakitoScrap.checkNewQusation():
-    sendText = sendText + '・' + test + '\n'
-  slack = slackweb.Slack(url=slackUrl)
-  slack.notify(text=sendText, username='sakitobot')
+  if(not len(questionList) == 0):
+    sendText = '新規アンケートが追加されました。\n'
+    for test in questionList:
+      sendText = sendText + '・' + test + '\n'
+    slack = slackweb.Slack(url=slackUrl)
+    slack.notify(text=sendText, username='sakitobot')
 
 if __name__ == '__main__':
   main()
